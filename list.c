60f624a5692e2df505d3f5b50716691a19df2640
Lorenz Gerber <genfys@gmail.com>
2016-08-31 22:47:49 +0200

    starting the project

---
+++ list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// typedef struct for the cell of a directed, single linked list
+typedef struct dlist {
+  *void data;
+  struct dlist *next;
+};
+
+// constructor of empty list
+
+// add a list element
+
+// check if the list is empty
+
+// inspect position
+
+// check if position is end
+
+// go to first position
+
+// go to next 
+
+// remove
